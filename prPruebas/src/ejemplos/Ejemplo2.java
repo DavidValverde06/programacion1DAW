292009329de6d6b416ec30f423b40a9fd9e6498a
1DAW <1DAW@DESKTOP-PU42LST>
2025-09-25 08:46:37 +0200

    Proyecto de pruebas de clase

---
+++ prPruebas/src/ejemplos/Ejemplo2.java
@@ -0,0 +1,15 @@
+package ejemplos;
+
+public class Ejemplo2 {
+
+	public static void main(String[] args) {
+
+		byte num1=7;
+		byte num2=10;
+		
+		int suma = num1+num2;
+		
+		System.out.println("El resultado de la suma de " + num1 + " y " + num2 + " es igual a " + suma + ".");
+	}
+
+}
