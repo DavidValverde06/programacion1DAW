693db30ff39ad8afde3ba9c23db8276636313d13
1DAW <1DAW@DESKTOP-PU42LST>
2025-10-09 14:56:45 +0200

    09/10/2025

---
+++ prPractica02/src/prPractica02/Ejercicioo41.java
@@ -0,0 +1,22 @@
+package prPractica02;
+
+import java.util.Scanner;
+
+/**
+ * 41. Introducir dos números inferiores a 50 y superiores a 0 por teclado. Al más pequeño de ellos lo
+	   aumentamos de 5 en 5 y al más grande lo disminuimos de 2 en 2. Imprimir ambas series alternativamente
+	   hasta que el menor supere al mayor.
+ */
+
+public class Ejercicioo41 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		
+		
+		sc.close();
+	}
+
+}
--- prPractica02/src/prPractica02/Ejercicioo49.java
+++ prPractica02/src/prPractica02/Ejercicioo49.java
@@ -1,5 +1,7 @@
 package prPractica02;
 
+import java.util.Scanner;
+
 /**
  * 49. Hacer un algoritmo que simule el funcionamiento de un reloj digital y que nos permita ponerlo en hora, 
 	   es decir, al comenzar a ejecutar el programa nos preguntará si deseamos ponerlo en hora, si 
@@ -9,9 +11,58 @@ package prPractica02;
 
 public class Ejercicioo49 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) /*throws InterruptedException*/ {
 
-		
+		Scanner sc = new Scanner(System.in);
+
+		char validacion;
+		int horas,minutos,segundos;
+
+		System.out.print("¿Desea poner el reloj en hora (S/N)? ");
+		validacion=sc.nextLine().charAt(0);
+		if (validacion=='s') {
+			System.out.print("Hora: ");
+			horas=sc.nextInt();
+			System.out.print("Minutos: ");
+			minutos=sc.nextInt();
+			System.out.print("Segundos: ");
+			segundos=sc.nextInt();
+			
+			for(int cont=0;cont>=0;cont++) {
+				//Thread.sleep(10);
+				System.out.println(horas+":"+minutos+":"+segundos);
+			}
+		}
+		else if (validacion=='n') {
+			horas=00;
+			minutos=00;
+			segundos=00;
+			
+			for(int cont=0;cont>=0;cont++) {
+				//Thread.sleep(500);
+				System.out.println(horas +":"+minutos +":"+segundos);
+
+				segundos++;
+				
+				if (segundos>59) {
+					minutos++;
+					segundos=0;
+
+					if (minutos>59) {
+						minutos=0;
+						horas++;
+						if (horas>23) {
+							horas=0;
+						}
+					}
+				}
+			}
+		}
+		else {
+			System.out.print("ERROR");
+		}
+
+		sc.close();
 	}
 
 }
