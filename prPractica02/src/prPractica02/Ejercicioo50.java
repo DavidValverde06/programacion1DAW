d0cdc86ca2f8868d5b0c49bcf78e8f7450aa211e
1DAW <1DAW@DESKTOP-PU42LST>
2025-10-10 10:50:10 +0200

    10/10/2025

---
+++ prPractica02/src/prPractica02/Ejercicioo50.java
@@ -0,0 +1,15 @@
+package prPractica02;
+
+/**
+ * 50. Simular el lanzamiento de una moneda al aire y que pare cuando salgan 3 caras consecutivas. Visualizar
+	   el número de tiradas realizadas.
+ */
+
+public class Ejercicioo50 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
