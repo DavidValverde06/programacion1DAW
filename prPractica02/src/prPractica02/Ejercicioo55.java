eebd9f18bd3dde6d67def1e4eb4217518acb8c1e
2daw <2daw@2daw04>
2025-10-28 11:03:44 +0100

    28/10/2025

--- prPractica02/src/prPractica02/Ejercicioo55.java
+++ prPractica02/src/prPractica02/Ejercicioo55.java
@@ -3,18 +3,18 @@ package prPractica02;
 /**
  * 55. Supongamos que una importante empresa de electrodomésticos nos contrata para resolver un problema
 		de logística. El primer caso práctico que nos plantean es el siguiente:
-		
+
 		En las grandes ciudades el precio del suelo es muy caro, por lo que comprar o alquilar grandes
 		superficies de almacenamiento de electrodomésticos resulta prohibitivo en el centro de la ciudad.
 		La solución es alejarse del casco urbano, sin embargo, cuanto más nos alejamos, más nos cuesta el
 		precio de distribución que cada día hay que abonar a los transportistas que nos trasladan los
 		electrodomésticos de la periferia al centro (donde se realizan la mayoría de las compras).
-		
+
 		La estrategia que adoptaremos es la siguiente:
-		
+
 		a. Adquirir un almacén pequeño en el centro de la ciudad (para 200 electrodomésticos, por termino
 		medio).
-		
+
 		b. Adquirir almacenes en anillos concéntricos de 5 km a partir del centro de la ciudad; cada almacén
 		podrá contener un stock del doble de electrodomésticos que el almacén anterior (es decir, de 400
 		electrodomésticos a 5 km del núcleo urbano de 800 electrodomésticos a 10 km, ...)
@@ -25,9 +25,23 @@ package prPractica02;
 
 public class Ejercicioo55 {
 
-	public static void main(String[] args) {
-		
-		sin hacer
-	}
+    public static void main(String[] args) {
 
+        int almacenKm=0;
+        int electrodomesticos=200;
+        int capacidadTotal=0;
+
+        while (electrodomesticos<100000) {
+            System.out.println("Almacén a " + almacenKm + " kilómetros con capacidad para " + electrodomesticos + " electrodomésticos");
+
+            capacidadTotal += electrodomesticos;
+
+            almacenKm+=5;
+            electrodomesticos*=2;
+        }
+
+        System.out.println("\nLa capacidad total de los almacenes es de: " + capacidadTotal + " electrodomésticos");
+
+    }
 }
+
