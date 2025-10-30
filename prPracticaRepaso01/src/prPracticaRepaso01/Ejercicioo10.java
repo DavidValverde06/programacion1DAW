8318e79da0f7d58147e4f95fb0a8462b475eec0c
2daw <2daw@2daw04>
2025-10-30 10:25:55 +0100

    30/10/2025

--- prPracticaRepaso01/src/prPracticaRepaso01/Ejercicioo10.java
+++ prPracticaRepaso01/src/prPracticaRepaso01/Ejercicioo10.java
@@ -10,30 +10,144 @@ import java.util.Scanner;
 public class Ejercicioo10 {
 
 	public static void main(String[] args) {
-		
-		pensar mejor
-		
-		int altura;
+
+		int altura,espacios,asteriscos;
 		String cadena="";
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Introduce la altura del triángulo: ");
 		altura=sc.nextInt();
-		
-		System.out.println();
-		
+
+		// ---------------------------------------------------------------------------------------------
+		// PRIMER TRIÁNGULO
+		System.out.println("\n----------------\nPrimer triángulo\n");
+
 		for (int cont=0;cont<altura;cont++) {
 			System.out.println(cadena+='*');
 		}
-		
-		System.out.println(); // SALTO DE LÍNEA
+
+		System.out.println("\n----------------");
+		// FIN PRIMER TRIÁNGULO
+		// ---------------------------------------------------------------------------------------------
+
+
+
+		// ---------------------------------------------------------------------------------------------
+		// SEGUNDO TRIÁNGULO
+		System.out.println("Segundo triángulo\n");
+
+		cadena="";
+
+		for (int cont=altura;cont>0;cont--) {
+
+			for (int cont2=0;cont2<cont;cont2++) {
+				cadena+='*';
+			}
+			cadena+='\n';
+		}
+
+		System.out.println(cadena);
+
+		System.out.println("----------------");
+		// FIN SEGUNDO TRIÁNGULO
+		// ---------------------------------------------------------------------------------------------
+
+
+
+		// ---------------------------------------------------------------------------------------------
+		// TERCER TRIÁNGULO
+
+		//		Línea 1: 4 espacios + 1 asterisco
+		//		Línea 2: 3 espacios + 3 asteriscos
+		//		Línea 3: 2 espacios + 5 asteriscos
+		//		Línea 4: 1 espacio + 7 asteriscos
+		//		Línea 5: 0 espacios + 9 asteriscos
+
+		cadena="";
+
+		System.out.println("Tercer triángulo\n");
+
+		for (int cont=0;cont<altura;cont++) {
+			cadena="";
+			espacios=altura-cont-1;
+			asteriscos=2*cont+1;
+			for (int cont2=0;cont2<espacios;cont2++) {
+				cadena+=' ';
+			}
+			for (int cont3=0;cont3<asteriscos;cont3++) {
+				cadena+='*';
+			}
+			System.out.println(cadena);
+		}
+
+		System.out.println("\n----------------");
+		// FIN TERCER TRIÁNGULO
+		// ---------------------------------------------------------------------------------------------
+
+
+
+		// ---------------------------------------------------------------------------------------------
+		// CUARTO TRIÁNGULO
+
+		// TRIÁNGULO DE ARRIBA
+
+		cadena="";
+
+		System.out.println("Cuarto triángulo\n");
+
+		for (int cont=0;cont<altura-1;cont++) {
+			cadena="";
+			espacios=altura-cont-1;
+			asteriscos=2*cont+1;
+			for (int cont2=0;cont2<espacios;cont2++) {
+				cadena+=' ';
+			}
+			for (int cont3=0;cont3<asteriscos;cont3++) {
+				cadena+='*';
+			}
+			System.out.println(cadena);
+		}
+
+		// TRIÁNGULO DE ABAJO (INVERTIDO)
+
+		//		Espacios: cont (empieza en 0 y sube)
+		//		Asteriscos: 2 * (altura - cont - 1) + 1
+
+		for (int cont=0;cont<altura;cont++) {
+			cadena="";
+			espacios=cont;
+			asteriscos=2*(altura-cont-1)+1;
+			for (int cont2=0;cont2<espacios;cont2++) {
+				cadena+=' ';
+			}
+			for (int cont3=0;cont3<asteriscos;cont3++) {
+				cadena+='*';
+			}
+			System.out.println(cadena);
+		}
+
+		System.out.println("\n----------------");
+		// FIN CUARTO TRIÁNGULO
+		// ---------------------------------------------------------------------------------------------
+
+
+
+		// ---------------------------------------------------------------------------------------------
+		// QUINTO TRIÁNGULO
+		System.out.println("\nQuinto triángulo\n");
+
+		falta este
 		
 		cadena="";
 		
 		for (int cont=0;cont<altura;cont++) {
-			System.out.println(cadena+=' ');
+			System.out.println(cadena+=cont);
 		}
-		
+
+		System.out.println("\n----------------");
+		// FIN QUINTO TRIÁNGULO
+		// ---------------------------------------------------------------------------------------------
+
 		sc.close();
 	}
 
