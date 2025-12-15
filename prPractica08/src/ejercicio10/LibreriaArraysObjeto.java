35eafa87beb9dabd6e9b5dc673f7233ad9de165c
1DAW <1DAW@DESKTOP-PU42LST>
2025-12-15 14:56:38 +0100

    15/12/2025

--- prPractica08/src/ejercicio10/LibreriaArraysObjeto.java
+++ prPractica08/src/ejercicio10/LibreriaArraysObjeto.java
@@ -1,5 +1,131 @@
 package ejercicio10;
 
+import java.util.Scanner;
+
+/**
+ * Librería con métodos de objeto para manejar arrays, el array con el 
+ * que trabajaremos será variable de instancia.
+ * 
+ * Esta librería incluirá los siguientes métodos:
+ *    - Carga de datos de teclado
+ *    - Carga de datos aleatoria
+ *    - toString
+ *    - Elemento máximo del array
+ *    - Elemento mínimo del array
+ *    - Suma de los elementos del array
+ *    - Métodos de búsqueda (lineal y dicotómica o binaria)
+ *    - Métodos varios de ordenación (burbuja, selección, inserción y Shell)
+ *    - Método que desordene el array
+ *    - Método de insertar por posición
+ *    - Método de borrar por posición
+ *    - Método de borrar por elemento
+ *    - Método para borrar todas las apariciones de un elemento
+ * 
+ * @author profesor
+ *
+ */
+
 public class LibreriaArraysObjeto {
 
+	private static final int NUM_ELEM_DEFECTO = 10;
+
+	/**
+	 * Variables de instancia
+	 */
+	private int [] array;
+
+	/**
+	 * Constructor
+	 */
+	public LibreriaArraysObjeto(int numElementos) {
+		this.array = new int[numElementos];
+	}
+
+	public LibreriaArraysObjeto() {
+		//this.array = new int[NUM_ELEM_DEFECTO];
+		this(NUM_ELEM_DEFECTO);
+	}
+
+	/**
+	 * Getter's y setter's
+	 */
+	public int[] getArray() {
+		return array;
+	}
+
+	public void setArray(int[] array) {
+		this.array = array;
+	}
+
+
+
+	/**
+	 * Método Carga de datos de teclado
+	 */
+	public void cargaDatosTeclado(Scanner sc) {
+		for (int cont=0;cont<array.length;cont++) {
+			System.out.print("Introduzca el elemento de la posición " + (cont+1) + ": ");
+			array[cont]=sc.nextInt();
+		}
+	}
+
+	/**
+	 * Método Carga de datos aleatoria
+	 */
+	public void cargaDatosAleatorio() {
+		for (int cont=0;cont<array.length;cont++) {
+			array[cont]=(int)(Math.random()*20)+1; // Números entre 1 y 20
+		}
+	}
+
+	/**
+	 * toString
+	 */
+	@Override
+	public String toString() {
+		String cadena = "Array: ";
+		for (int cont=0;cont<array.length;cont++) {
+			cadena+=array[cont] + " ";
+		}
+		return cadena;
+	}
+
+	public String toString2() {
+		String cadena = "Array: ";
+		for (int cont=0;cont<array.length;cont++) {
+			if (cont!=array.length-1) {
+				cadena+=array[cont] + ",";
+			}
+			else {
+				cadena+=array[cont];
+			}
+		}
+		return cadena;
+	}
+
+	/**
+	 * Método Elemento máximo del array.
+	 */
+	public int elementoMax() {
+		int max=array[0];
+		for (int cont=0;cont<array.length;cont++) {
+			if (array[cont]>max) {
+				max=array[cont];
+			}
+		}
+		return max;
+	}
+
+	/**
+	 * Método Elemento mínimo del array
+	 */
+	public int elementoMin() {
+		int min=array[0];
+		for (int cont=0;cont<array.length;cont++) {
+			if (array[cont]<min) {
+				min=array[cont];
+			}
+		}
+		return min;
+	}
 }
---
+++ prPractica08/src/ejercicio10/TestLibreriaObjeto.java
@@ -0,0 +1,67 @@
+package ejercicio10;
+
+import java.util.Scanner;
+
+public class TestLibreriaObjeto {
+
+	public static void main(String[] args) {
+
+		byte opcion;
+
+		LibreriaArraysObjeto ob = new LibreriaArraysObjeto(8);
+
+		try (Scanner sc = new Scanner(System.in)) {
+
+			do {
+				menu();
+
+				System.out.print("\nElija opción: ");
+				opcion = sc.nextByte();
+
+				switch (opcion) {
+				case 1 -> ob.cargaDatosTeclado(sc);
+				case 2 -> ob.cargaDatosAleatorio();
+				case 3 -> System.out.println(ob.toString2());
+				case 4 -> System.out.println("Valor máximo: " + ob.elementoMax());
+				case 5 -> System.out.println("Valor mínimo: " + ob.elementoMin());
+				case 6 ->
+				case 7 ->
+				case 8 ->
+				case 9 ->
+				case 10 ->
+				case 11 ->
+				case 12 ->
+				case 13 ->
+				case 14 ->
+				case 15 ->
+				case 16 ->
+				case 17 -> System.out.println("\nSaliendo del programa...");
+				default -> System.out.println("Opción no válida");
+				}
+			}
+			while (opcion!=17);
+
+		}
+	}
+
+	public static void menu() {
+		System.out.println("\n --- MENU DE OPERACIONES ---");
+		System.out.println("1.- Carga de datos desde teclado");
+		System.out.println("2.- Carga de datos aleatoria");
+		System.out.println("3.- Mostrar array");
+		System.out.println("4.- Valor máximo del array");
+		System.out.println("5.- Valor mínimo del array");
+		System.out.println("6.- Suma de los elementos del array");
+		System.out.println("7.- Insertar por posición");
+		System.out.println("8.- Borrar por posición");
+		System.out.println("9.- Borrar buscando el número");
+		System.out.println("10.- Búsqueda lineal");
+		System.out.println("11.- Búsqueda binaria o dicotómica");
+		System.out.println("12.- Ordenación por el método de la burbuja");
+		System.out.println("13.- Ordenación por el método de inserción");
+		System.out.println("14.- Ordenación por el método de selección");
+		System.out.println("15.- Ordenación por el método de Shell");
+		System.out.println("16.- Desordenar array");
+		System.out.println("17.- Salir");
+	}
+}
