1e1249dd326478c07cbe0392ddcb8a18828dfa40
1DAW <1DAW@DESKTOP-PU42LST>
2026-01-12 14:55:42 +0100

    12/01/2026

--- prPractica08/src/arraysUnidimensionales/Ejercicioo11.java
+++ prPractica08/src/arraysUnidimensionales/Ejercicioo11.java
@@ -11,17 +11,22 @@ import java.util.Scanner;
 
 public class Ejercicioo11 {
 
-	sin hacer
-	
 	public static void main(String[] args) {
 		byte opcion;
 
-		int[] polinomio1 = new int[4];
-		int[] polinomio2 = new int[2];
-
 		try (Scanner sc = new Scanner(System.in)) {
 
+			int[] pol1 = cargarPolinomio(sc);
+			int[] pol2 = cargarPolinomio(sc);
+			int[] polResultado = new int[Math.max(pol1.length, pol2.length)];
+
 			do {
+				System.out.print("Polinomio 1: ");
+				mostrarPolinomio(pol1);
+
+				System.out.print("Polinomio 2: ");
+				mostrarPolinomio(pol2);
+
 				menu();
 				System.out.print("\nElija una opción: ");
 				opcion = sc.nextByte();
@@ -34,13 +39,48 @@ public class Ejercicioo11 {
 				case 4 -> System.out.println("\nSaliendo del programa...");
 				default -> System.out.println("Opción no válida");
 				}
-				if (opcion != 4) {
+
+				if (opcion!=4) {
 					System.out.println("\nPulsa Enter para mostrar el menú");
 					sc.nextLine(); // Pulsar Enter para mostrar el menú
 				}
 			}
 			while(opcion!=4);
 		}
+	}
+
+	public static int[] cargarPolinomio(Scanner sc) {
+		int grado;
+
+		System.out.print("Introduce el grado del polinomio: ");
+		grado = Math.abs(sc.nextInt());
+
+		int[] pol = new int [grado+1];
+
+		for (int cont=grado;cont>=0;cont--) {
+			System.out.print("Introduce el grado " + cont + ": ");
+			pol[cont]=sc.nextInt();
+		}
+
+		System.out.println();
+
+		return pol;
+	}
+
+	public static void mostrarPolinomio(int[] polinomio) {
+		for (int cont=polinomio.length-1;cont>=0;cont--) {
+			System.out.print(polinomio[cont] + "x^" + cont + " ");
+		}
+		System.out.println();
+	}
+
+	public static int[] sumar(int[] pol1, int[] pol2) {
+
+	}
+
+
+	public static int[] multiplicar(int[] pol1, int[] pol2) {
+
 	}
 
 	public static void menu() {
