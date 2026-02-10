2f9c4582a5955cfc0b187d06167b04db787fc59e
DavidValverde06 <davidvalverdebarea2006@gmail.com>
2026-02-10 22:53:02 +0100

    10/02/2026

--- prPractica10/src/ejercicio9/Ejercicio9.java
+++ prPractica10/src/ejercicio9/Ejercicio9.java
@@ -1,5 +1,7 @@
 package ejercicio9;
 
+import java.util.*;
+
 /**
  * 9.- Crea un diccionario de términos informáticos y su definición, con la ayuda de las colecciones. El diccionario
  * deberá estar ordenado por clave de forma alfabética. Para probarlo, pide al usuario un término y
@@ -8,9 +10,26 @@ package ejercicio9;
 public class Ejercicio9 {
 
 	public static void main(String[] args) {
-		
-		sin hacer
 
+		Map<String, String> dic = new HashMap<>();
+
+		String termino="";
+
+		try (Scanner sc = new Scanner(System.in)) {
+
+			dic.put("hardware", "parte fisica del ordenador");
+			dic.put("software", "programas del ordenador");
+			dic.put("cpu", "procesa la informacion");
+			dic.put("ram", "memoria temporal");
+			dic.put("sistema operativo", "controla el ordenador");
+			dic.put("algoritmo", "pasos para resolver un problema");
+			dic.put("programa", "conjunto de instrucciones");
+
+			System.out.println(dic);
+
+			System.out.print("Introduce un término: ");
+			termino=sc.next();
+		}
 	}
 
 }
