7099638931ac45bbac9eafbaf85e7e6f4616b293
1DAW <1DAW@DESKTOP-PU42LST>
2026-02-11 14:55:41 +0100

    11/02/2026

---
+++ prEjemplosExcepciones/src/ejemplo3/ExPropia.java
@@ -0,0 +1,25 @@
+package ejemplo3;
+/**
+ * Definión de una clase que define una clase propia de manejo de excepciones
+ */
+
+public class ExPropia extends Exception 
+{ 
+	private static final long serialVersionUID = -3286997316340530132L;
+
+	/**
+	 * Constructor por defecto
+	 */
+	public ExPropia() { 
+		super("Esta es mi propia excepcion");	
+	}
+
+	/**
+	 * Constructor con mensaje personalizado
+	 * @param s
+	 */
+	public ExPropia(String s) { 
+		super(s); 
+	}
+}
+
