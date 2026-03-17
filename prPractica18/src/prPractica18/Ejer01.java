7415886ea793e4ed8914285e881d0f1c71ac52c0
1DAW <1DAW@DESKTOP-PU42LST>
2026-03-17 10:29:14 +0100

    17/03/2026

--- prPractica18/src/prPractica18/Ejer01.java
+++ prPractica18/src/prPractica18/Ejer01.java
@@ -1,29 +1,32 @@
 package prPractica18;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class Ejer01 extends JFrame {
+public class Ejer01 extends JFrame implements FocusListener {
 
 	private static final long serialVersionUID = -2739471212745483337L;
 
 	public Ejer01() {
+
 		super("Ejemplo con FocusListener");
 
 		Container cp = this.getContentPane();
 
 		// El panel principal será un BorderLayout, en el que tendremos:
-		JPanel panelPrincipal = new JPanel();
-		
+		JPanel panelPrincipal = new JPanel(new BorderLayout());
+
 		// En la parte norte un panel con un campo de texto, una etiqueta, un combo y un botón.
 		JPanel panelNorte = new JPanel();
 		JTextField tfCampoTexto = new JTextField("Campo de texto");
 		JLabel jlEtiqueta = new JLabel("Una etiqueta");
-		
+
 		String[] opcionesCombo = new String[15];
 		for (int cont=0;cont<opcionesCombo.length;cont++) {
 			opcionesCombo[cont] = "Selecciona item #" + cont;
 		}
+
 		JComboBox<String> miCombo = new JComboBox<>(opcionesCombo);
 		JButton bBoton = new JButton("Botón");
 
@@ -32,19 +35,34 @@ public class Ejer01 extends JFrame {
 		panelNorte.add(miCombo);
 		panelNorte.add(bBoton);
 
-		// En el centro un JList, contenido en un JScrollPane.
-		JPanel panelCentro = new JPanel();
-		
-		// En el sur un JTextArea, contenida en un JScrollPane, al JTextArea le he dado un tamaño de 10x5.
-		JPanel panelSur = new JPanel();
-		
-		JTextArea atPanelSur = new JTextArea(10,5);
-		JScrollPane scrollPanelSur = new JScrollPane();
-		
+		// En el centro un JList, contenido en un JScrollPane.		
+		String[] listaArray = new String[15];
+		for (int cont=0;cont<listaArray.length;cont++) {
+			listaArray[cont] = "Lista item #" + cont;
+		}
+
+		JList<String> miJList = new JList<>(listaArray);
+		JScrollPane scrollPanelCentro = new JScrollPane(miJList);
+
+		// En el sur un JTextArea, contenida en un JScrollPane, al JTextArea le he dado un tamaño de 10x5.		
+		JTextArea textAreaSur = new JTextArea(10,5);
+		JScrollPane scrollPanelSur = new JScrollPane(textAreaSur);
+
+		// Añadir el control de foco a todos los componentes de la ventana, excepto a los contenedores
+		tfCampoTexto.addFocusListener(this);
+		jlEtiqueta.addFocusListener(this);
+		miCombo.addFocusListener(this);
+		bBoton.addFocusListener(this);
+		miJList.addFocusListener(this);
+		textAreaSur.addFocusListener(this);
+
+
+		// Añadir todo al Panel Principal
 		panelPrincipal.add(panelNorte,BorderLayout.NORTH);
-		panelPrincipal.add(panelCentro,BorderLayout.CENTER);
-		panelPrincipal.add(panelSur,BorderLayout.SOUTH);
-		cp.add(panelPrincipal);
+		panelPrincipal.add(scrollPanelCentro,BorderLayout.CENTER);
+		panelPrincipal.add(scrollPanelSur,BorderLayout.SOUTH);
+
+		cp.add(panelPrincipal,BorderLayout.CENTER);
 	}
 
 	public static void main(String[] args) {
@@ -60,6 +78,44 @@ public class Ejer01 extends JFrame {
 		ventana.setLocationRelativeTo(null);
 
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	/**
+	 * En cuanto al funcionamiento de la aplicación, lo que vamos a controlar es el evento de foco
+	 * (FocusEvent), así que vais a tener que implementar la interfaz FocusListener. Vamos a añadir el control
+	 * de foco (addFocusListener), a todos los componentes que tenemos en la ventana, excepto a los
+	 * contenedores.
+	 * 
+	 * - Cuando un componente gane el foco, añadiremos (método append) al JTextArea un texto en el que
+	 * ponga “Foco ganado” e información del componente que ha generado el evento, método
+	 * getComponent(), sobre el parámetro que os llega al método (FocusEvent e), que en realidad es el
+	 * componente que generado el evento.
+	 * 
+	 * - De la misma forma cuando un componente pierda el foco, haréis lo mismo, pero cambiando el
+	 * texto de “Foco ganado” por “Foco perdido”.
+	 * 
+	 * - Observar que el caso de la etiqueta no hace absolutamente nada, este componente no genera
+	 * eventos tipo FocusEvent.
+	 * 
+	 * - Asimismo, comprobar también que cada vez que cambiáis de componente, se producen dos
+	 * eventos, el que genera el componente que pierde el foco, y el que genera el componente que lo
+	 * gana. Si os ayuda, podéis poner “escribes” en la consola, para que comprobéis como se van
+	 * sucediendo los eventos, aunque creo que en este caso, no es necesario, ya que lo estáis mostrando
+	 * en el JTextArea.
+	 * 
+	 * - Observar también que el método getComponent os devuelve mucha información del componente,
+	 * en el caso del JComboBox, incluso la opción que habéis elegido.
+	 */
+	@Override
+	public void focusGained(FocusEvent e) {
+
+		System.out.println("Foco ganado: " + e.getComponent());
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+
+		System.out.println("Foco perdido: " + e.getComponent());
 	}
 
 }
---
+++ prPractica18/src/prPractica18/Ejer02.java
@@ -0,0 +1,198 @@
+package prPractica18;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+/**
+ * 2.- En este segundo ejercicio, os voy a proponer un juego sencillo para jugar con el ordenador a lanzar una
+ * par de dados aleatoriamente. Programaremos un evento tipo Action (ActionEvent), que se lanzará al
+ * pulsar el botón Lanzar dados:
+ */
+public class Ejer02 extends JFrame {
+
+	private static final long serialVersionUID = 4589263424739830124L;
+
+	public Ejer02() {
+
+		super("Jugar a los dados");
+
+		this.setIconImage(new ImageIcon("dados.gif").getImage());
+
+		Container cp = this.getContentPane();
+
+		/**
+		 * Respecto a la distribución de componentes y paneles, yo he seguido la siguiente, aunque podéis
+		 * buscar otras alternativas:
+		 * 
+		 * - El panel principal (contentPane) es un BorderLayout, en el que se ha colocado en el norte el panel
+		 * con la puntuación, en el centro otro panel con la apuesta del jugador y el resultado, y en el sur un
+		 * último panel que contiene el botón de lanzamiento.
+		 */
+		JPanel panelPrincipal = new JPanel(new BorderLayout());
+
+
+		// ----------- //
+		// Panel Norte //
+		// ----------- //
+
+		/**
+		 * El panel de la puntuación es un panel FlowLayout con alineación a la derecha, en el que he
+		 * colocado la etiqueta con el texto Puntos obtenidos, y otra etiqueta, con el número 0, inicialmente,
+		 * y que conforme el juego comience a funcionar se irá actualizando. El tipo de fuente que he usado
+		 * para estas dos etiquetas es Comic Sans Ms, en negrita y con un tamaño de 15.
+		 * 
+		 * Font fuente = new Font("Comic Sans MS", Font.BOLD, 15);
+		 */
+		JPanel panelPuntuacionNorte = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+		JLabel etiquetaPuntosObtenidos = new JLabel("Puntos Obtenidos: ");
+		JLabel puntos = new JLabel("0");
+
+		Font fuente = new Font("Comic Sans MS", Font.BOLD, 15);
+
+		etiquetaPuntosObtenidos.setFont(fuente);
+		etiquetaPuntosObtenidos.setForeground(Color.RED);
+
+		puntos.setFont(fuente);
+		puntos.setForeground(Color.RED);
+
+		panelPuntuacionNorte.add(etiquetaPuntosObtenidos);
+		panelPuntuacionNorte.add(puntos);
+
+
+		// ------------ //
+		// Panel Centro //
+		// ------------ //
+
+		/**
+		 * En cuanto al panel con la Apuesta del jugador y el Resultado, es un GridLayout(1,2), es decir una
+		 * fila con dos celdas, así me aseguro que sean iguales. A su vez en cada una de esas celdas tendrá
+		 * otro panel pero esta vez con gestor de esquemas GridLayout(2,1). Estos dos paneles llevarán
+		 * como estáis viendo un borde compuesto con relieve y ell texto “Apuesta Jugador” en el primero, y
+		 * “Resultado” en el segundo.
+		 * 
+		 * pApuesta.setBorder(new CompoundBorder(
+		 * new TitledBorder("Apuesta Jugador"),
+		 * new BevelBorder(BevelBorder.RAISED)));
+		 */
+		JPanel panelJuegoCentro = new JPanel(new GridLayout(1,2));
+
+		JPanel apuestaJugador = new JPanel(new GridLayout(2,1));
+
+		apuestaJugador.setBorder(new CompoundBorder(
+				new TitledBorder("Apuesta Jugador"),
+				new BevelBorder(BevelBorder.RAISED)));
+
+		JPanel resultado = new JPanel(new GridLayout(2,1));
+
+		resultado.setBorder(new CompoundBorder(
+				new TitledBorder("Resultado"),
+				new BevelBorder(BevelBorder.RAISED)));
+
+		/**
+		 * Lo que veis en estos paneles en color blanco, son dos JTextField con borde de título, y en ese
+		 * borde es donde especifico “Dado 1” y “Dado 2”. La fuente de estos JTextField es la misma que la
+		 * de las etiquetas de la Puntuación obtenida. 
+		 */
+		JTextField dado1Apuesta = new JTextField();
+		dado1Apuesta.setBorder(new CompoundBorder(
+				new TitledBorder("Dado 1"), null));
+		dado1Apuesta.setFont(fuente);
+
+		JTextField dado2Apuesta = new JTextField();
+		dado2Apuesta.setBorder(new CompoundBorder(
+				new TitledBorder("Dado 2"), null));
+		dado2Apuesta.setFont(fuente);
+
+		JTextField dado1Resultado = new JTextField();
+		dado1Resultado.setBorder(new CompoundBorder(
+				new TitledBorder("Dado 1"), null));
+		dado1Resultado.setFont(fuente);
+
+		JTextField dado2Resultado = new JTextField();
+		dado2Resultado.setBorder(new CompoundBorder(
+				new TitledBorder("Dado 2"), null));
+		dado2Resultado.setFont(fuente);
+
+		// Añadir los TextFields a su panel correspondiente
+		apuestaJugador.add(dado1Apuesta);
+		apuestaJugador.add(dado2Apuesta);
+
+		resultado.add(dado1Resultado);
+		resultado.add(dado2Resultado);
+
+		/**
+		 * Otra cosa a tener en cuenta con respecto a los JTextField, es que los del primer panel “Apuesta
+		 * jugador”, son normales, es decir, editables, pero los del panel del “Resultado” no nos interesa que
+		 * sean editables, ya que simplemente los utilizaremos para mostrar el resultado de generar
+		 * aleatoriamente los dados, y no queremos que el usuario escriba en ellos, para que no sean
+		 * editables tenéis el método setEditable, pero al quitarle la edición os lo podrá en color gris, así que
+		 * también tendréis que cambiar el color a blanco. (Algunas estaréis pensando que porque no he
+		 * utilizado JLabel en este último panel en lugar de armar este lio, el motivo es porque no se veían
+		 * iguales los JLabel que los JTextField y quería conseguir un aspecto más uniforme).
+		 */
+
+		// Cambiar el TextField para que no sea editable (se pondrá de color gris)
+		dado1Resultado.setEditable(false);
+		dado2Resultado.setEditable(false);
+
+		// Cambiar el color de fondo a blanco
+		dado1Resultado.setBackground(Color.WHITE);
+		dado2Resultado.setBackground(Color.WHITE);
+
+		// Añadir los paneles al Panel Central
+		panelJuegoCentro.add(apuestaJugador);
+		panelJuegoCentro.add(resultado);
+
+
+		// --------- //
+		// Panel Sur //
+		// --------- //
+
+		/**
+		 * Por último, el botón tiene una imagen (es un gif animado), a continuación os pongo una de las
+		 * formas de poder poner una imagen a un botón, también le he puesto un borde al botón para que
+		 * quede más levantado, quedaba regular tras ponerle la imagen.
+		 * 
+		 * bLanza = new JButton("Lanzar dados",
+		 * 							new ImageIcon("recursos/dados-03.gif"));
+		 * 
+		 * bLanza.setBorder(new BevelBorder(BevelBorder.RAISED));
+		 */
+		JPanel panelBotonSur = new JPanel();
+
+		JButton bLanza = new JButton("Lanzar dados");
+
+		bLanza = new JButton("Lanzar dados", new ImageIcon("dados-03.gif"));
+		bLanza.setBorder(new BevelBorder(BevelBorder.RAISED));
+
+		panelBotonSur.add(bLanza);
+
+		// Añadir todo al Panel Principal
+		panelPrincipal.add(panelPuntuacionNorte,BorderLayout.NORTH);
+		panelPrincipal.add(panelJuegoCentro,BorderLayout.CENTER);
+		panelPrincipal.add(panelBotonSur,BorderLayout.SOUTH);
+
+		cp.add(panelPrincipal);
+	}
+
+	public static void main(String[] args) {
+
+		Ejer02 ventana = new Ejer02();
+
+		ventana.pack();
+
+		ventana.setResizable(false);
+
+		ventana.setVisible(true);
+
+		// Tamaño (300, 250)
+		ventana.setSize(300, 250);
+
+		ventana.setLocationRelativeTo(null);
+
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
